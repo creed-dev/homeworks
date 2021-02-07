493fbb3d9dc8ba3e42eaffdb11a2ad10db39ae49
Creed <creed-dev@internet.ru>
2021-02-07 18:18:49 +0700

    Add 5-th homework(update)

--- JS1/lesson5/script.js
+++ JS1/lesson5/script.js
@@ -2,9 +2,7 @@ function chess() {
     var table = document.createElement("table");
 var tr,td;
 var flag = true;
-var arr = {
-    0: ['a','b','c','d','e','f','g','h'],
-};
+var arrLetters = ['A','B','C','D','E','F','G','H'];
 let figure = {
     0 : ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-150px -16px','-372px -17px', '-484px -22px','-263px -19px'],
     1 : ['-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px','-595px -19px', '-595px -19px','-595px -19px'],
@@ -12,13 +10,16 @@ let figure = {
     7 : ['-263px -116px', '-484px -116px', '-372px -116px', '-150px -116px', '-150px -116px','-372px -116px', '-484px -116px','-263px -116px'],
 };
 
-for(var i = 0;i < 8;i++) {
+for(var i = 0;i < 9;i++) {
     tr = document.createElement("tr");
     var td = document.createElement('td');
     td.innerHTML = 8 - i;
+    if (i == 8) {
+        td.innerText = "";
+    }
     tr.appendChild(td);
     table.append(tr);
-    for(var j = 0;j < 8;j++) {
+    for(var j = 0, k = 0;j < 8;j++, k++) {
         if(j == 0) flag = !flag;
         td = document.createElement("td");
         if (flag) td.setAttribute("style", "border: 1px solid black; padding: 35px; background: brown;");
@@ -27,6 +28,10 @@ for(var i = 0;i < 8;i++) {
             td.style.backgroundImage = 'url(сhess_symbols_set_.png)';
             td.style.backgroundPosition = figure[i][j];
         }
+        if (i == 8) {
+            td.innerHTML = arrLetters[k];
+            td.setAttribute("style", "text-align: center;");
+        } 
         tr.append(td);
         flag = !flag;
     }
